0609a0b181c3922f9c576cf6c56ca10bb318da42
kmrulhsan <dauntlesskmrul@gmail.com>
2024-11-06 17:51:57 +0600

    basics content practiced

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
  //this is first js code //
- console.log('Hello World');
+//  console.log('Hello World');
+
+//practice using variables//
+// let name = 'Kamrul';
+// console.log(name);
+
+// const interestRate = 0.3;
+// // interestRate = 0.3;
+// console.log(interestRate);
+
+//primitive type values
+// let name = 'hasan'; //string literal
+// let age = 25; //number literal
+// let isMarried = false; //boolean literal
+// let names = undefined;
+// let selectColor = null;
+
+//objects
+// let person = {
+//     name: 'hasan',
+//     age: 25
+// };
+// // Dot notation
+// person.name = 'Kamrul';
+
+// // Bracket notation
+// let selection = 'age';
+// person[selection] = 30;
+
+//arrays
+// let selectedColor = ['red', 'blue'];
+// selectedColor[2] = 4;
+// console.log(selectedColor.length);
+
+//function performing a task
+function greet(name, lastName){
+    console.log('Hello'+ ' ' + name + ' '+ lastName);
+}
+// greet('Kamrul', 'Hasan');
+
+//function for calculting
+function square(number){
+    return number * number;
+}
+console.log(square(3));
